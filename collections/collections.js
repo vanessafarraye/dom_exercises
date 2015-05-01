d9b7525271ff64a856d292844ef46624d50d3998
Vanessa Farraye <vanessafarraye@Vanessas-MacBook-Air-2.local>
2015-04-30 22:36:40 -0700

    homeworkDay4

--- collections/collections.js
+++ collections/collections.js
@@ -2,17 +2,19 @@
 // Part 1
 //
 
+var lis = document.querySelectorAll("li")
+for(var i = 0; i < lis.length; i++) {
+    lis[i].style.borderBottom = "1px solid black"
+}
+//part2
+ var dataType = document.querySelectorAll("li.datatype")
+ for(var i = 0; i<dataType.length; i++) {
+     dataType[i].style.color = "red"
+ }
+//part3
+ var funcLi = document.querySelectorAll("li.function")
+ for(var i = 0; i<funcLi.length; i++) {
+     funcLi[i].style.color = "blue"
+ }
 
-
-
-//
-// Part 2
-//
-
-
-
-
-//
-// Part 3
-//
 
--- events/events.js
+++ events/events.js
@@ -1,1 +1,22 @@
 // Your code here
+ var bottonClicker = document.querySelector("#clicker");
+bottonClicker.addEventListener("click", function (event) {
+    alert("hello");
+});
+
+
+var test = document.getElementById("mouser")
+test.addEventListener("mouseleave", function( event ) {   
+    event.target.style.backgroundColor = "green";
+    console.log("testBlue")
+})
+test.addEventListener("mouseover", function( event ) {   
+    event.target.style.backgroundColor = "yellow";
+    console.log("test")
+})
+
+var form = document.querySelector("form")
+form.addEventListener("submit", function( event ) { 
+var value = document.querySelector("input").value  
+    alert(value)
+})
